9ad76157be7bf7f291d2fcd4703ee9e60902c6d3
root <root@DESKTOP-VM6OKQU.localdomain>
2025-10-31 11:47:03 +0530

    Jenkins Script

---
+++ jenkins.sh
@@ -0,0 +1,6 @@
+sudo dnf install java-17-amazon-corretto-devel
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install jenkins
+sudo systemctl daemon-reload
